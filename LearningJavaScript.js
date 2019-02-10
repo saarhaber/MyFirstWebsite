626c5b3688a548caf52768444304b887a31e68fb
Saar Haber <37675905+saarhaber@users.noreply.github.com>
2019-02-10 00:39:00 -0500

    website

--- LearningJavaScript.js
+++ LearningJavaScript.js
@@ -1,7 +1,5 @@
-<script>
 //this is a comment
-alert("This is an \'alert\' box!");
+alert("This is an alert box!");
 var price = 55.55;
 document.write(price);
 // 2/10/19 - learned loops.
-</script>
