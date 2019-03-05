b9cd45b76127a0dbbb67b38bb918f177462d1e62
Saar Haber <37675905+saarhaber@users.noreply.github.com>
2019-03-05 18:13:10 -0500

    added nav-bar

--- javascript.js
+++
@@ -1,7 +0,0 @@
-//this is a comment
-alert ("This is an alert box!");
-var price = 55.55;
-document.write(price);
-// 2/10/19 - learned loops.
-// 2/13/19 codecamp - learned loops, object, ststments.
-//not different from any other oop
